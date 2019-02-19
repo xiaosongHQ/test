5addcf018ddeba6a60b337b406d37b2630d16c91
Your Name <you@example.com>
2019-02-19 16:38:24 +0800

    asd

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<script>!function(e){function t(a){if(i[a])return i[a].exports;var n=i[a]={exports:{},id:a,loaded:!1};return e[a].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var i={};return t.m=e,t.c=i,t.p="",t(0)}([function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var i=window;t["default"]=i.flex=function(normal,e,t){var a=e||100,n=t||1,r=i.document,o=navigator.userAgent,d=o.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i),l=o.match(/U3\/((\d+|\.){5,})/i),c=l&&parseInt(l[1].split(".").join(""),10)>=80,p=navigator.appVersion.match(/(iphone|ipad|ipod)/gi),s=i.devicePixelRatio||1;p||d&&d[1]>534||c||(s=1);var u=normal?1:1/s,m=r.querySelector('meta[name="viewport"]');m||(m=r.createElement("meta"),m.setAttribute("name","viewport"),r.head.appendChild(m)),m.setAttribute("content","width=device-width,user-scalable=no,initial-scale="+u+",maximum-scale="+u+",minimum-scale="+u),r.documentElement.style.fontSize=normal?"50px": a/2*s*n+"px"},e.exports=t["default"]}]);  flex(false,100, 1);</script>
-
-<script src="http://libs.baidu.com/jquery/2.1.4/jquery.min.js"></script>
 <?php
    	
    	require_once('./vendor/autoload.php');
@@ -15,39 +12,12 @@ $config = [
 ];
 
 $app = Factory::officialAccount($config);
-$accessToken = $app->access_token;
-        $token = $accessToken->getToken(true);
-        $ticket = json_decode(file_get_contents("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token={$token['access_token']}&type=jsapi"));
-        $jsapi_ticket = $ticket->ticket;
-        $noncestr = 'ws'.mt_rand(1111,9999);
-        $timestamp = time();
-        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        //$url = "http://www.huodongge.cn";
-        $string = "jsapi_ticket={$jsapi_ticket}&noncestr={$noncestr}&timestamp={$timestamp}&url={$url}";
-        $signature = sha1($string);
-        $signPackage = array(
-            "jsapi_ticket" =>$jsapi_ticket,
-            "appId"     => "wxca28f17bb360d36b",
-            "nonceStr"  => $noncestr,
-            "timestamp" => $timestamp,
-            "url"       => $url,
-            "signature" => $signature,
-            "rawString" => $string,
-            'jsApiList'=> [
-                'onMenuShareTimeline',
-                'onMenuShareAppMessage'
-            ]
-        );
-
-
-
-//$baseJson = $app->jssdk->buildConfig(array('onMenuShareTimeline','onMenuShareAppMessage'), true);
-
-//ar_dump($baseJson);
+$signPackage = $app->jssdk->buildConfig(array('onMenuShareTimeline','onMenuShareAppMessage'), true);
+//var_dump($signPackage);
 
 ?>
 <button id="share">分享测试</button>
+<script src="http://libs.baidu.com/jquery/2.1.4/jquery.min.js"></script>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 <script>
   /*
@@ -61,12 +31,13 @@ $accessToken = $app->access_token;
    * 邮件主题：【微信JS-SDK反馈】具体问题
    * 邮件内容说明：用简明的语言描述问题所在，并交代清楚遇到该问题的场景，可附上截屏图片，微信团队会尽快处理你的反馈。
    */
+var baseJson = $.parseJSON(<?php echo json_encode($signPackage); ?>);
   wx.config({
     debug: true,
-    appId: '<?php echo $signPackage["appId"];?>',
-    timestamp: <?php echo $signPackage["timestamp"];?>,
-    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
-    signature: '<?php echo $signPackage["signature"];?>',
+    appId: baseJson.appId,
+    timestamp: baseJson.timestamp,
+    nonceStr: baseJson.nonceStr,
+    signature: baseJson.signature,
     jsApiList: [
       'onMenuShareTimeline',
                 'onMenuShareAppMessage'
