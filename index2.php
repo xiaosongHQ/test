fc974109df1b23f45c0958748475de46f948b000
xiaosongHQ <1520339193@qq.com>
2019-02-19 15:56:36 +0800

    gad

--- index2.php
+++ index2.php
@@ -1,53 +1,17 @@
-<script>!function(e){function t(a){if(i[a])return i[a].exports;var n=i[a]={exports:{},id:a,loaded:!1};return e[a].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var i={};return t.m=e,t.c=i,t.p="",t(0)}([function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var i=window;t["default"]=i.flex=function(normal,e,t){var a=e||100,n=t||1,r=i.document,o=navigator.userAgent,d=o.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i),l=o.match(/U3\/((\d+|\.){5,})/i),c=l&&parseInt(l[1].split(".").join(""),10)>=80,p=navigator.appVersion.match(/(iphone|ipad|ipod)/gi),s=i.devicePixelRatio||1;p||d&&d[1]>534||c||(s=1);var u=normal?1:1/s,m=r.querySelector('meta[name="viewport"]');m||(m=r.createElement("meta"),m.setAttribute("name","viewport"),r.head.appendChild(m)),m.setAttribute("content","width=device-width,user-scalable=no,initial-scale="+u+",maximum-scale="+u+",minimum-scale="+u),r.documentElement.style.fontSize=normal?"50px": a/2*s*n+"px"},e.exports=t["default"]}]);  flex(false,100, 1);</script>
-
-<script src="http://libs.baidu.com/jquery/2.1.4/jquery.min.js"></script>
 <?php
-   	
-   	require_once('./vendor/autoload.php');
-
-   	use EasyWeChat\Factory;
-
-$config = [
-    'app_id' => 'wxeb8503aed05a2c1a',
-    'secret' => '052836c4dde956a0644acf0607c8934d',
-    // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
-    'response_type' => 'array',
-];
-
-$app = Factory::officialAccount($config);
-$accessToken = $app->access_token;
-        $token = $accessToken->getToken(true);
-        $ticket = json_decode(file_get_contents("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token={$token['access_token']}&type=jsapi"));
-        $jsapi_ticket = $ticket->ticket;
-        $noncestr = 'ws'.mt_rand(1111,9999);
-        $timestamp = time();
-        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        //$url = "http://www.huodongge.cn";
-        $string = "jsapi_ticket={$jsapi_ticket}&noncestr={$noncestr}&timestamp={$timestamp}&url={$url}";
-        $signature = sha1($string);
-        $signPackage = array(
-            "jsapi_ticket" =>$jsapi_ticket,
-            "appId"     => "wxca28f17bb360d36b",
-            "nonceStr"  => $noncestr,
-            "timestamp" => $timestamp,
-            "url"       => $url,
-            "signature" => $signature,
-            "rawString" => $string,
-            'jsApiList'=> [
-                'onMenuShareTimeline',
-                'onMenuShareAppMessage'
-            ]
-        );
-
-
-
-//$baseJson = $app->jssdk->buildConfig(array('onMenuShareTimeline','onMenuShareAppMessage'), true);
-
-//ar_dump($baseJson);
-
+require_once "jssdk.php";
+$jssdk = new JSSDK("wxeb8503aed05a2c1a", "052836c4dde956a0644acf0607c8934d");
+$signPackage = $jssdk->GetSignPackage();
 ?>
-<button id="share">分享测试</button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  
+</body>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 <script>
   /*
@@ -108,4 +72,4 @@ $accessToken = $app->access_token;
                   });
   });
 </script>
-</script>
+</html>
